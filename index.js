defb1e50e960e5db16b7da0f4ba0b6fc467a1c6d
Polina Novoselova <polka.nov@mail.ru>
2023-06-24 18:57:32 +0200

    add html and css

---
+++ index.js
@@ -0,0 +1,8 @@
+const randomTextNode = document.getElementById('random-text');
+const btnFindActivityNode = document.getElementById('btn-find-activity');
+
+btnFindActivityNode.addEventListener('click', findActivity());
+
+const findActivity = () => {
+    fetch
+}
